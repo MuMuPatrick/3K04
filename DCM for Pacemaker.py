56ad525b0f0b995f6ad81254260319bfd86b2c8e
Zixuan(Patrick) Pan <44325915+MuMuPatrick@users.noreply.github.com>
2018-11-20 17:19:01 -0800

    Update DCM for Pacemaker.py

--- DCM for Pacemaker.py
+++ DCM for Pacemaker.py
@@ -4,29 +4,17 @@
 
 
 from tkinter import *
+from tkinter import messagebox
 
 from Modifier import *
 
 import os
 
-creds = 'UserInfo.txt' # text file to store the User information
-creds2 = 'Parameter List' # text file to store the Parameter List   
 
-global LRL_Input
-global URL_Input
-global AtrAmp_Input
-global APW_Input
-global VtrAmp_Input
-global VPW_Input
-global VSEN_Input
-global AtrSen_Input
-global ARP_Input
-global VRP_Input
-global PVARP_Input
-global Hysteresis_Input
-global HYS_Input
-global RSM_Input
-global RateSmoothing_Input
+
+
+creds = 'UserInfo.txt' # text file to store the User information 
+
 
 
 """
@@ -58,13 +46,15 @@ def Welcome():
 """
     
  
-def Signup(): 
-    global pwordE 
-    global nameE
+def Signup():
     global roots
+    global nameE
+    global pwordE
     
-    if os.path.isfile(creds): # The existence of the file shows we are access the Signup Screen by Login Screen. So, we need to destroy the Login Screen
+    if os.access("Users",os.F_OK): # The existence of the file shows we are access the Signup Screen by Login Screen. So, we need to destroy the Login Screen
         rootA.destroy()
+    else:
+        os.mkdir('Users')
     roots = Tk() 
     roots.title('Signup')
     instruction = Label(roots, text='Please Enter new Credidentials\n') 
@@ -93,32 +83,30 @@ def Signup():
 """
 
 def FSSignup():
-    if os.path.isfile(creds): #Check the existence of the file. If file exists, we need to append the file with new user's information, else we create a new user information file
-        f=open(creds, 'r')
-        data = f.readlines() 
-        num = len(data)
-      
-        f.close
-        if (num >= 20):
-            nk = Tk()
-            instruction = Label(nk,text = "Maximum Number of Users !!!!")
-            instruction.grid(row=0)
-        else:
-            file2=open(creds,'a')
-            file2.write(nameE.get()) 
-            file2.write('\n')
-            file2.write(pwordE.get())
-            file2.write('\n')
-            file2.close()
-            Login()
+    
+    os.chdir('./Users')
+    readfiles = os.listdir(os.getcwd())
+    num = len(readfiles)
+    User = nameE.get()
+    Password = pwordE.get()
+    
+    if num >= 20:
+        messagebox.showerror("System Message", "Maximun Number of Users !!!")
+    elif os.access(User,os.F_OK):
+        messagebox.showerror("System Message", "User has already existed !!!")
     else:
+        os.mkdir(User)
+        os.chdir('./'+ User)
         f=open(creds, 'w')
-        f.write(nameE.get()) 
+        f.write(User) 
         f.write('\n')
-        f.write(pwordE.get())
+        f.write(Password)
         f.write('\n')
-        f.close() 
-    roots.destroy() # After transmittion is finished, destroy the Signup Screen 
+        f.close()
+        os.chdir('..')
+    
+    roots.destroy() # After transmittion is finished, destroy the Signup Screen
+    os.chdir('..')
     Login()
 
     
@@ -134,10 +122,10 @@ def FSSignup():
 """
 
 def Login():
-    global nameEL
-    global pwordEL 
     global rootA
- 
+    global nameEL
+    global pwordEL
+    
     rootA = Tk() 
     rootA.title('Login') 
  
@@ -178,38 +166,23 @@ def Login():
 
  
 def CheckLogin():
-    global r
-    Check = 0
-    with open(creds,'r') as f:
-        data = f.readlines() 
-        num = len(data)
-        for i in range(int(num/2)):
-            uname = data[i*2].rstrip() 
-            pword = data[i*2+1].rstrip() 
-            if nameEL.get() == uname and pwordEL.get() == pword:
-                Check = 1
-                break
-    if Check == True: 
-        r = Tk() 
-        r.title('System Message')
-        instruction = Label(r, text='\n Logged In')
-        instruction.grid(row=0,columnspan=3)
-        r2b2 = Button(r,text='Next',command=ModesControlPanel)
-        r2b2.grid(row=1,columnspan=3)
+    os.chdir('./Users')
+    User = nameEL.get()
+    Password = pwordEL.get()
+    if os.access(User,os.F_OK) == FALSE :
+        messagebox.showerror('System Message', 'Invalid Username')
     else:
-        r = Tk()
-        r.title('System Message')
-        instruction = Label(r, text='\n[!] Invalid Login')
-        instruction.grid(row=0,sticky=W)
-        r1b2 = Button(r, text='back',command = destroy_r)
-        r1b2.grid(row=1,sticky=W)
-
-"""
-@brief:Destroy the System Message Screen
-"""
-
-def destroy_r():
-    r.destroy()
+        os.chdir('./' + User)
+        with open(creds,'r') as f:
+            data = f.readlines()
+            TruePword = data[1]
+            f.close()
+        os.chdir('..')
+        os.chdir('..')
+        if TruePword != Password+'\n':
+            messagebox.showerror('System Message', 'Invalid Password')
+        else:
+            ModesControlPanel()
 
 """
 @brief: Check which screen to go after the Welcome Screen (If No user information, go to Signup Screen first)
@@ -217,9 +190,9 @@ def destroy_r():
 
 def Enter():
     wel.destroy()
-    if os.path.isfile(creds):
+    if os.access('Users',os.F_OK):
         Login()
-    else: 
+    else:
         Signup()
 
 """
@@ -231,48 +204,67 @@ def Enter():
 def ModesControlPanel():
     global mcp
     rootA.destroy();
-    destroy_r();
     mcp = Tk() 
     mcp.title('Modes')
     mcp.geometry("200x350")
+
     OFF_Button = Button(mcp, text='OFF', command=OFF_Mode_Modifier) 
     OFF_Button.grid(row=1,column=1)
+    
     AOO_Button = Button(mcp, text='AOO', command=AOO_Mode_Modifier) 
     AOO_Button.grid(row=1,column=2)
+    
     VOO_Button = Button(mcp, text='VOO', command=VOO_Mode_Modifier) 
     VOO_Button.grid(row=1,column=3)
+    
     DDI_Button = Button(mcp, text='DDI', command=OFF_Mode_Modifier) 
     DDI_Button.grid(row=2,column=1)
+    
     DOO_Button = Button(mcp, text='DOO', command=OFF_Mode_Modifier) 
     DOO_Button.grid(row=2,column=2)
+    
     DDD_Button = Button(mcp, text='DDD', command=OFF_Mode_Modifier) 
     DDD_Button.grid(row=2,column=3)
+    
     AAI_Button = Button(mcp, text='AAI', command=AAI_Mode_Modifier) 
     AAI_Button.grid(row=3,column=1)
+    
     VDD_Button = Button(mcp, text='VDD', command=OFF_Mode_Modifier) 
     VDD_Button.grid(row=3,column=2)
+    
     VVI_Button = Button(mcp, text='VVI', command=VVI_Mode_Modifier) 
     VVI_Button.grid(row=3,column=3)
+    
     AAT_Button = Button(mcp, text='AAT', command=OFF_Mode_Modifier) 
     AAT_Button.grid(row=4,column=1)
+    
     VVT_Button = Button(mcp, text='VVT', command=OFF_Mode_Modifier) 
     VVT_Button.grid(row=4,column=2)
+    
     DDDR_Button = Button(mcp, text='DDDR', command=OFF_Mode_Modifier) 
     DDDR_Button.grid(row=4,column=3)
+    
     VDDR_Button = Button(mcp, text='VDDR', command=OFF_Mode_Modifier) 
     VDDR_Button.grid(row=5,column=1)
+    
     DDIR_Button = Button(mcp, text='DDIR', command=OFF_Mode_Modifier) 
     DDIR_Button.grid(row=5,column=2)
+    
     DOOR_Button = Button(mcp, text='DOOR', command=OFF_Mode_Modifier) 
     DOOR_Button.grid(row=5,column=3)
+    
     AOOR_Button = Button(mcp, text='AOOR', command=OFF_Mode_Modifier) 
     AOOR_Button.grid(row=6,column=1)
+    
     AAIR_Button = Button(mcp, text='AAIR', command=OFF_Mode_Modifier) 
     AAIR_Button.grid(row=6,column=2)
+    
     VOOR_Button = Button(mcp, text='VOOR', command=OFF_Mode_Modifier) 
     VOOR_Button.grid(row=6,column=3)
+    
     VVIR_Button = Button(mcp, text='VVIR', command=OFF_Mode_Modifier) 
     VVIR_Button.grid(row=7,column=1)
+    
     spL = Label(mcp, text = "")
     spL.grid(row=8,columnspan=1,column=2)
     cc_Button = Button(mcp, text='Check\nConnection',command=checkConnection) 
